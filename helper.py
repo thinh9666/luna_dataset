73347ab61615c2443db441087706e71a3d6d5487
thinh9666 <idenityv@gmail.com>
2026-06-17 17:03:46 +0700

    update training.py

--- helper.py
+++ helper.py
@@ -4,7 +4,7 @@ log =  logging.getLogger(__name__) #tạo logger có tên là helper
 def run(app_cls, *argv):#run(LunaTrainingApp, '--epochs=1')
     #app_cls là class app tôi muốn chạy, *argv là các tham số truyền thêm
     argv = list(argv)#biến tuple thành list argv =['--epochs=1']
-    argv.insert(0, '--num-workers=4') #argv = ['--num-workers=4', '--epochs=1']
+    argv.insert(0, '--num-workers=2') #argv = ['--num-workers=4', '--epochs=1']
 
     log.info(f"Running: {app_cls.__name__}({argv!r}).main()")
     #Running: LunaTrainingApp(['--num-workers=4', '--epochs=1']).main()
